06678cac4329df963e5e9f918cd9e36a1766fcef
Glavo <zjx001202@gmail.com>
2026-04-19 03:16:21 +0800

    Add publishing and test configuration to build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
     id("java-library")
+    id("jacoco")
+    id("maven-publish")
+    id("signing")
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("org.glavo.load-maven-publish-properties") version "0.1.0"
 }
 
 group = "org.glavo"
-version = "1.0-SNAPSHOT"
-val mainClassName = "org.glavo.javafx.webp.WebPViewerApp"
+version = "0.1.0" + "-SNAPSHOT"
+description = "WebP decoding library for JavaFX"
 
 val osName = System.getProperty("os.name").lowercase()
 val javafxPlatform = when {
@@ -38,7 +43,10 @@ dependencies {
 java {
     modularity.inferModulePath.set(true)
     withSourcesJar()
+    withJavadocJar()
 }
+
+tasks.withType()
 
 val mainSourceSet = sourceSets.named("main")
 
@@ -47,9 +55,30 @@ tasks.withType<JavaCompile> {
     options.release.set(17)
 }
 
+val mainClassName = "org.glavo.javafx.webp.WebPViewerApp"
+
 tasks.jar {
-    manifest {
-        attributes["Main-Class"] = mainClassName
+    manifest.attributes(
+        "Main-Class" to mainClassName
+    )
+}
+
+tasks.javadoc {
+    (options as StandardJavadocDocletOptions).also {
+        it.jFlags!!.addAll(listOf("-Duser.language=en", "-Duser.country=", "-Duser.variant="))
+
+        it.encoding("UTF-8")
+        it.addStringOption("link", "https://docs.oracle.com/en/java/javase/25/docs/api/")
+        it.addBooleanOption("html5", true)
+        it.addStringOption("Xdoclint:none", "-quiet")
+
+        it.tags!!.addAll(
+            listOf(
+                "apiNote:a:API Note:",
+                "implNote:a:Implementation Note:",
+                "implSpec:a:Implementation Specification:",
+            )
+        )
     }
 }
 
@@ -63,4 +92,75 @@ tasks.register<JavaExec>("run") {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging.showStandardStreams = true
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.withType<GenerateModuleMetadata> {
+    enabled = false
+}
+
+publishing.publications.create<MavenPublication>("maven") {
+    groupId = project.group.toString()
+    version = project.version.toString()
+    artifactId = project.name
+
+    from(components["java"])
+
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        url.set("https://github.com/Glavo/javafx-webp")
+
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("Glavo")
+                name.set("Glavo")
+                email.set("zjx001202@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/Glavo/javafx-webp")
+        }
+    }
+}
+
+if (rootProject.ext.has("signing.key")) {
+    signing {
+        useInMemoryPgpKeys(
+            rootProject.ext["signing.keyId"].toString(),
+            rootProject.ext["signing.key"].toString(),
+            rootProject.ext["signing.password"].toString(),
+        )
+        sign(publishing.publications["maven"])
+    }
+}
+
+// ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+
+            username.set(rootProject.ext["sonatypeUsername"].toString())
+            password.set(rootProject.ext["sonatypePassword"].toString())
+        }
+    }
 }
